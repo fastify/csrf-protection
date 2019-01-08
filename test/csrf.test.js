727ea179d429316f4ec102a1195767c905669307
Tarang11 <tarangdixit93@gmail.com>
2019-01-08 13:20:29 +0530

    gitignore

--- test/csrf.test.js
+++ test/csrf.test.js
@@ -4,15 +4,3 @@ const Fastify = require('fastify');
 const fastifyCookie = require('fastify-cookie');
 const fastifyCSRF = require('../lib/fastifyCsrf');
 
-test('a simple test', ()=>{
-	const fastify = Fastify({logger: true});
-	const options = { cookie: true };
-	fastify.register(fastifyCookie);
-	fastify.register(fastifyCSRF, options);
-	fastify.listen(3000, err =>{
-		if(err) {
-			process.exit(0)
-		}
-		console.log('Listening on port 3000');
-	});
-})
