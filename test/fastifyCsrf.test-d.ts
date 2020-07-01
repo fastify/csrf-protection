15d6d5224dc43c2665d00c44668a6cd2b239605e
Maximilian Karacsony <maximilian.karacsony@mail.de>
2020-07-01 20:58:11 +0200

    Add test for type definitions

---
+++ test/fastifyCsrf.test-d.ts
@@ -0,0 +1,26 @@
+import * as fastify from "fastify";
+import * as fastifyCookie from "fastify-cookie";
+import * as fastifyCSRF from "../lib/fastifyCsrf";
+
+const server = fastify();
+server.register(fastifyCookie);
+server.register(fastifyCSRF, {
+  cookie: true,
+  ignoreMethods: ["GET"],
+  key: "_custom_csrf",
+  value: (req) => {
+    return req.body["some_key"];
+  },
+});
+
+server.get("/", (request, reply) => {
+  var form = `
+      <form method = "post" action="/data">
+         <input type="text" name"field_name"/>
+         <input type="hidden" value="${request.csrfToken()}"
+	 name="_csrf /* this token can be sent in request header as well */"/>
+         <button type="submit">Submit</button>
+      </form>
+    `;
+  reply.type("text/html").send(form);
+});
