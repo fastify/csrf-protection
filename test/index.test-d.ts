5fbba35c01d679e704e2554a2708225f322e3f60
delvedor <tommydelved@gmail.com>
2020-10-10 12:43:15 +0200

    Typescript support

---
+++ test/index.test-d.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify'
+import FastifyCookie from 'fastify-cookie'
+import FastifyCsrf from '..'
+
+async function run () {
+  const fastify = Fastify()
+  await fastify.register(FastifyCookie)
+  await fastify.register(FastifyCsrf)
+
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: async (req, reply) => {
+      return reply.generateCsrf()
+    }
+  })
+
+  fastify.route({
+    method: 'POST',
+    url: '/',
+    onRequest: fastify.csrfProtection,
+    handler: async (req, reply) => {
+      return req.body
+    }
+  })
+}
