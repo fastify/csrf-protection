a565ce33be75c2668925d2238840bdd6615b4245
delvedor <tommydelved@gmail.com>
2020-10-10 11:55:53 +0200

    Updated test

--- test/basic.test.js
+++ test/basic.test.js
@@ -18,7 +18,13 @@ test('Cookies', t => {
     await fastify.register(fastifyCsrf)
     return fastify
   }
-  runTest(t, load)
+  runTest(t, load, { property: '_csrf', place: 'query' })
+  runTest(t, load, { property: '_csrf', place: 'body' }, 'preValidation')
+  runTest(t, load, { property: 'csrf-token', place: 'headers' })
+  runTest(t, load, { property: 'xsrf-token', place: 'headers' })
+  runTest(t, load, { property: 'x-csrf-token', place: 'headers' })
+  runTest(t, load, { property: 'x-xsrf-token', place: 'headers' })
+  runCookieOpts(t, load)
   t.end()
 })
 
@@ -33,7 +39,12 @@ test('Fastify Session', t => {
     await fastify.register(fastifyCsrf, { sessionPlugin: 'fastify-session' })
     return fastify
   }
-  runTest(t, load, 'preValidation')
+  runTest(t, load, { property: '_csrf', place: 'query' }, 'preValidation')
+  runTest(t, load, { property: '_csrf', place: 'body' }, 'preValidation')
+  runTest(t, load, { property: 'csrf-token', place: 'headers' }, 'preValidation')
+  runTest(t, load, { property: 'xsrf-token', place: 'headers' }, 'preValidation')
+  runTest(t, load, { property: 'x-csrf-token', place: 'headers' }, 'preValidation')
+  runTest(t, load, { property: 'x-xsrf-token', place: 'headers' }, 'preValidation')
   t.end()
 })
 
@@ -44,12 +55,72 @@ test('Fastify Secure Session', t => {
     await fastify.register(fastifyCsrf, { sessionPlugin: 'fastify-secure-session' })
     return fastify
   }
-  runTest(t, load)
+  runTest(t, load, { property: '_csrf', place: 'query' })
+  runTest(t, load, { property: '_csrf', place: 'body' }, 'preValidation')
+  runTest(t, load, { property: 'csrf-token', place: 'headers' })
+  runTest(t, load, { property: 'xsrf-token', place: 'headers' })
+  runTest(t, load, { property: 'x-csrf-token', place: 'headers' })
+  runTest(t, load, { property: 'x-xsrf-token', place: 'headers' })
+  runCookieOpts(t, load)
   t.end()
 })
 
-function runTest (t, load, hook = 'onRequest') {
-  t.test('Token in query', async t => {
+test('Validation', t => {
+  t.test('cookieKey', t => {
+    t.plan(1)
+    const fastify = Fastify()
+    fastify.register(fastifyCookie)
+    fastify.register(fastifyCsrf, { cookieKey: 42 })
+    fastify.ready(err => {
+      t.strictEqual(err.message, 'cookieKey should be a string')
+    })
+  })
+
+  t.test('sessionKey', t => {
+    t.plan(1)
+    const fastify = Fastify()
+    fastify.register(fastifyCookie)
+    fastify.register(fastifyCsrf, { sessionKey: 42 })
+    fastify.ready(err => {
+      t.strictEqual(err.message, 'sessionKey should be a string')
+    })
+  })
+
+  t.test('getToken', t => {
+    t.plan(1)
+    const fastify = Fastify()
+    fastify.register(fastifyCookie)
+    fastify.register(fastifyCsrf, { getToken: 42 })
+    fastify.ready(err => {
+      t.strictEqual(err.message, 'getToken should be a function')
+    })
+  })
+
+  t.test('cookieOpts', t => {
+    t.plan(1)
+    const fastify = Fastify()
+    fastify.register(fastifyCookie)
+    fastify.register(fastifyCsrf, { cookieOpts: 42 })
+    fastify.ready(err => {
+      t.strictEqual(err.message, 'cookieOpts should be a object')
+    })
+  })
+
+  t.test('sessionPlugin', t => {
+    t.plan(1)
+    const fastify = Fastify()
+    fastify.register(fastifyCookie)
+    fastify.register(fastifyCsrf, { sessionPlugin: 42 })
+    fastify.ready(err => {
+      t.strictEqual(err.message, "sessionPlugin should be one of the following: 'fastify-cookie', 'fastify-session', 'fastify-secure-session'")
+    })
+  })
+
+  t.end()
+})
+
+function runTest (t, load, tkn, hook = 'onRequest') {
+  t.test(`Token in ${tkn.place}`, async t => {
     const fastify = await load()
 
     fastify.get('/', async (req, reply) => {
@@ -70,17 +141,43 @@ function runTest (t, load, hook = 'onRequest') {
     const cookie = response.cookies[0]
     const token = response.json().token
 
-    response = await fastify.inject({
-      method: 'POST',
-      path: `/?_csrf=${token}`,
-      payload: { hello: 'world' },
-      cookies: {
-        [cookie.name]: cookie.value
-      }
-    })
+    if (tkn.place === 'query') {
+      response = await fastify.inject({
+        method: 'POST',
+        path: `/?${tkn.property}=${token}`,
+        payload: { hello: 'world' },
+        cookies: {
+          [cookie.name]: cookie.value
+        }
+      })
+    } else if (tkn.place === 'body') {
+      response = await fastify.inject({
+        method: 'POST',
+        path: '/',
+        payload: {
+          hello: 'world',
+          [tkn.property]: token
+        },
+        cookies: {
+          [cookie.name]: cookie.value
+        }
+      })
+    } else {
+      response = await fastify.inject({
+        method: 'POST',
+        path: '/',
+        payload: { hello: 'world' },
+        headers: {
+          [tkn.property]: token
+        },
+        cookies: {
+          [cookie.name]: cookie.value
+        }
+      })
+    }
 
     t.strictEqual(response.statusCode, 200)
-    t.deepEqual(response.json(), { hello: 'world' })
+    t.match(response.json(), { hello: 'world' })
 
     response = await fastify.inject({
       method: 'POST',
@@ -102,5 +199,24 @@ function runTest (t, load, hook = 'onRequest') {
 
     t.strictEqual(response.statusCode, 403)
     t.match(response.json(), { message: 'Invalid csrf token' })
+  })
+}
+
+function runCookieOpts (t, load) {
+  t.test('Custom cookie options', async t => {
+    const fastify = await load()
+
+    fastify.get('/', async (req, reply) => {
+      const token = await reply.generateCsrf({ path: '/hello' })
+      return { token }
+    })
+
+    const response = await fastify.inject({
+      method: 'GET',
+      path: '/'
+    })
+
+    const cookie = response.cookies[0]
+    t.match(cookie, { path: '/hello' })
   })
 }
