29c2a77e806dbc3aae5f724c1f90c3f472df5eda
Matteo Collina <hello@matteocollina.com>
2022-09-02 10:39:50 +0200

    Clarify that the preHandler lifecycle must be used if the token is passed as a body
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ example.mjs
@@ -0,0 +1,75 @@
+import Fastify from "fastify";
+import fastifyCookie from "@fastify/cookie";
+import fastifyCsrfProtection from "./index.js";
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.register(fastifyCookie);
+await fastify.register(fastifyCsrfProtection);
+
+
+fastify.post(
+  "/",
+  {
+    preHandler: [
+      async function (req) {
+        console.log(req.cookies);
+      },
+      fastify.csrfProtection
+    ]
+  },
+  async (req, reply) => {
+    return req.body;
+  }
+);
+
+
+// generate a token
+fastify.route({
+  method: "GET",
+  url: "/",
+  handler: async (req, reply) => {
+    const token = await reply.generateCsrf();
+    reply.type("text/html");
+    // return { token: token };
+
+    return `
+      <html>
+      <script type='text/javascript'>
+      async function test(event) {
+        event.preventDefault()
+        const rawResponse = await fetch('/', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ _csrf: '${token}'})
+        });
+        const content = await rawResponse.json();
+        
+        alert(JSON.stringify(content));
+      }
+     </script>
+        <body>
+          <form action="/" method='POST' id='form' onsubmit={test(event)}>
+            <input type='submit' value='submit' />
+          </form>
+ 
+        </body>
+      </html>
+    
+    `;
+  }
+});
+
+// Run the server!
+fastify.listen({ port: 3001 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  // Server is now listening on ${address}
+});
